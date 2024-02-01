49d5583ac39996b0e256efee57a8f85fd9f133f4
Oğuz Karan <oguzkaran@csystem.org>
2024-02-01 21:26:20 +0300

    Update

--- src/src-console/csd/App.java
+++ src/src-console/csd/App.java
@@ -3,6 +3,9 @@ package csd;
 class App {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello, World");
+		System.out.println(10 / 0)
+		System.out.println("Merhaba");
 	}
 }
+
+
