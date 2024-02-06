b561a042764e68381fa58a9a54becbee7462ca5d
Oğuz Karan <oguzkaran@csystem.org>
2024-02-06 22:56:20 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,31 +1,7 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir Java programı genel olarak paketlerden (package), paketler sınıflardan (class), sınıflar da metotlardan (method)
-	oluşur. Paket bildiriminin genel biçimi:
-	
-		package <isim>;
-		
-	Burada isim daha sonra ele alacağımız değişken isimlendirme kurallarına uygun bir isim olmalıdır. Paketler konusu
-	çok detaylı bir konudur, ileride detaylı olarak ele alınacaktır. Biz ilgili konuya kadar yalnızca csd paketi içerisinde
-	kodlama yapacağız. 
-	
-	Sınıf bildiriminin genel biçimi şu şekildedir:
-	
-	[bildirime ilişkin bazı anahtar sözcükler] class <isim> {
-		//...
-	}
-	
-	Burada isim değişken isimlendirme kurallarına uygun olmalıdır. Sınıf kavramı da oldukça geniş bir kavramdır. Sınıf bildiriminin
-	detayları konular içerisinde ele alınacaktır. Sınıf kavramı Nesne Yönelimli Programlama Tekiniğinin de temel taşlarından
-	biridir.  
-	
-	Java'da alt programlara metot denir. Metot bildirimi (method declaration) alt programın kodlarının yazılmasıdır. Bir 
-	metot bildirildiğinde kodlarının çalıştırılabilmesi için çağrılması (call/invoke) gerekir. Metot bildiriminin genel
-	biçimi şu şekildedir:
-	
-	[erişim belirleyici] [static] <geri dönüş değeri bilgisi> <isim>([parametre listesi])
-	{
-		//...
-	}
+	Çağıran metodun (caller) ait olduğu sınıf ile çağrılan metodun (callee) ait olduğu sınıf aynı paket içerisindeyse 
+	çağrı sırasında paket ismi kullanılmasına gerek yoktur. Çağıran metot ile çağrılan metot aynı sınıf içerisindeyse
+	ve paket ismi çağrıda kullanılmamışsa sınıf ismi de yazmaya gerek yoktur. Aşağıdaki demo örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -33,14 +9,32 @@ class App {
 	public static void main(String [] args)
 	{
 		System.out.println("Hello, World");
+		Sample.foo();
+		Mample.bar();
+		System.out.println("Goodbye, World");
 	}
 }
 
 
 class Sample {
 	//...
+	public static void foo()
+	{
+		tar();
+		System.out.println("foo");
+		Mample.bar();
+	}
+	
+	public static void tar()
+	{
+		System.out.println("tar");
+	}
 }
 
 class Mample {
 	//...
+	public static void bar()
+	{
+		System.out.println("bar");
+	}
 }
