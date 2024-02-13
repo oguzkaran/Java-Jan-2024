aa585675f11f211ee6edc4866814926bd517c059
Oğuz Karan <oguzkaran@csystem.org>
2024-02-14 00:10:49 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,38 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Farklı faaliyet alanları içerisinde aynı isimde yerel değişken bildirimi geçerlidir 
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
-
 	public static void main(String[] args) 
 	{
-		int a;
-			
+		
 	}
 }
 
-class Sample {
-	public static void foo()
-	{
-		int a;
-	
 
-		{
-			int b;
-			//..
-		}
-		
-		int b;
-		
-		//...
-	}
-	
-	public static void bar()
-	{
-		int a;
-		
-		//...
-	}
-}
 
