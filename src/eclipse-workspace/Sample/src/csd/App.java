a8e5350cc4f03d45b979624894987353dacea8fa
Oğuz Karan <oguzkaran@csystem.org>
2024-02-20 22:58:17 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,38 +1,32 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki demo örnekte add metodunun geri dönüş değeri getInt metodunun geri dönüş değeri yapılmıştır. Yani, getInt
-	metodu add metodu çağrıldıktan sonra elde edilen değere geri dönmektedir
+	Anahtar Notlar: Java'da 3 yerde atama işlemi yapılır: 
+	1. Yalın atama işlemi
+	2. Metodun geri dönüş değerinin geçici değişkene atanması
+	3. Argümandan parametre değişkene aktarım
+	
+	Özel bazı durumlar dışında atama işlemine ilişkin kurallar tüm atama işlemleri için geçerlidir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args) 
 	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("İki sayı giriniz:");
+		int x = kb.nextInt();
+		int y = kb.nextInt();		
 		int result;
 		
-		result = Sample.getInt();
+		result = Util.add(x + 2, y * 2);
 		
-		System.out.println(result);						
-	}
-}
-
-class Sample {
-	public static int getInt()
-	{
-		System.out.println("İki tane tamsayı değeri girmeniz gerekiyor:");
-		
-		return Util.add();
+		System.out.println(result);
 	}
 }
 
 class Util {
-	public static int add()
+	public static int add(int a, int b)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("İki sayı giriniz:");
-		int a = kb.nextInt();
-		int b = kb.nextInt();
-		
 		return a + b;
 	}
 }
