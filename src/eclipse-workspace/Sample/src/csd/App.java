e9d53c91653bee541b534165fdfc0782122b2439
Oğuz Karan <oguzkaran@csystem.org>
2024-02-29 22:36:03 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,40 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı double türden iki noktaya ilişkin koordinat bilgilerine göre iki nokta
-	arasındaki Euclid uzaklığına geri dönen distance isimli meotdu PointUtil isimli sınıf içerisinde yazınız ve aşağıdaki
-	kod ile test ediniz
-	
-	Euclid Uzaklığı fomulü: sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2) 
-	Açıklama: ^ üs anlamına gelmektedir 
+	Carriage Return karakter sabiti. 
+	Dikkat: Aşağıdaki örneği işletim sisteminizin "komut yorumlayıcı" programı ile çalıştırınız. Kullandığınız IDE'nin
+	console penceresi farklı yorumlayabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args) 
 	{
-		PointUtilDistanceTest.run();
-	}
-}
-
-class PointUtilDistanceTest {
-	public static void run() 
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
+		char c;
 		
-		System.out.print("İki noktanın koordinatlarını giriniz:");
-		double x1 = kb.nextDouble();
-		double y1 = kb.nextDouble();
-		double x2 = kb.nextDouble();
-		double y2 = kb.nextDouble();
-		double d = PointUtil.distance(x1, y1, x2, y2);
+		c = '\r';
 		
-		System.out.printf("distance(%f, %f, %f, %f) = %f%n", x1, y1, x2, y2, d);
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class PointUtil {
-	public static double distance(double x1, double y1, double x2, double y2)
-	{
-		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+		System.out.print("Hello\nWorld");
+		
 	}
 }
