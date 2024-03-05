7371c8a702b2108cc86c39f561cc638816dc8bfb
Oğuz Karan <oguzkaran@csystem.org>
2024-03-05 20:55:57 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,18 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Carriage Return karakter sabiti. 
-	Dikkat: Aşağıdaki örneği işletim sisteminizin "komut yorumlayıcı" programı ile çalıştırınız. Kullandığınız IDE'nin
-	console penceresi farklı yorumlayabilir
+	Sabitlerin bilimsel/üstel gösterilişi. Bu şekilde yazılan sabitler double türdendir  
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args) 
-	{
-		char c;
-		
-		c = '\r';
-		
-		System.out.print("Hello\nWorld");
+	{		
 		
 	}
 }
