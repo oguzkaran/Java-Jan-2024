8d6eb69bf711bae975a58e45c37cd29ad5485825
Oğuz Karan <oguzkaran@csystem.org>
2024-03-14 22:54:23 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,13 +1,29 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Noktalı virgül Java'da sonlandırıcı (terminator) karakter olarak kullanılır. Java'da başka terminator karakter yoktur.
-	Noktalı virgül genel olarak ifadeleri sonlandırmak ve bazı deyimleri de sonlandırmak için kullanılır.
+	Deyimler (Statements): Programın çalıştırılan parçalarına denir. Aslında bir program deyimlerin çalıştırılmasıyla
+	çalışır. Java'da deyimler şunlardır:
+	- Basit deyimler (simple statements): Bir ifadenin sonuna noktalı virgül konmasıyla oluşan deyimdir. örneğin:
+		System.out.println("Merhaba"); ya da x = y + z;
+	  Basit bir deyim çalıştırıldığında deyime ilişkin ifade hesaplanır.
+	  
+	- Bileşik deyimler (compound statements): { ve } arasında kalan yani bir blok içerisinde yazılan deyimlerdir. 
+	Bileşik deyim çalıştırıldığında blok içerisindeki deyimler yukarıdan aşağıya çalıştırılır.  Bu anlamda metodun
+	gövdesi de aslında bileşik olarak düşünülebilir.
+	
+	- Bildirim deyimleri (declaration statements): Bir değişkenin bildirildiği deyimdir. Bildirim deyimi çalıştırıldığında
+	bellekte yer ayrılır.
+	
+	- Kontrol deyimleri (control statements): Akışa yön veren deyimlerdir. Her kontrol deyiminin kendine özgü çalıştırılma
+	biçimi vardır. 
+	
+	- Boş deyim (null/empty statement): Noktalı virgülün tek başına kullanılması durumunda oluşan deyimdir. Boş deyim
+	çalıştırıldığında hiç bir şey yapılmaz		
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		
+					
 	}
 }
 
