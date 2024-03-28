09b38a2734a04b7ed14fb45e110332f56dfcfb27
Oğuz Karan <oguzkaran@csystem.org>
2024-03-28 23:30:40 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,44 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
- 	Aşağıdaki örnekte do-while döngü deyimi ile sıfır sayısı için 1 değerine geri dönülmüştür
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{	
-		NumberUtilcountDigitsTest.run();
+	{			
+				
 	}
 }
 
-class NumberUtilcountDigitsTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);		
-		
-		while (true) {
-			System.out.print("Bir sayı giriniz:");
-			int val = Integer.parseInt(kb.nextLine());
-			
-			System.out.printf("%d sayısının basamak sayısı:%d%n", val, NumberUtil.countDigits(val));
-			
-			if (val == 0)
-				return;
-		}
-		
-		
-	}
-}
-
-class NumberUtil {
-	public static int countDigits(int val)
-	{
-		int count = 0;
-		
-		do {
-			++count;
-			val /= 10;
-		} while (val != 0);		
-		
-		return count;
-	}
-}
