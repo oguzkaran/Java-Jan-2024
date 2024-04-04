4c9b79a0112e1f4db6f2736aa6daf622e0e1b4fe
Oğuz Karan <oguzkaran@csystem.org>
2024-04-04 21:38:09 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,12 +1,59 @@
 /*----------------------------------------------------------------------------------------------------------------------
+	Sınıf Çalışması: Parametresi ile aldığı long türden bir sayının asal olup olmadığını test eden isPrime isimli
+	metodu NumberUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz
 	
+	Kural: Bir sayı karekökünden küçük olan hiç bir asal sayıya tam olarak bölünemiyorsa asaldır.
+	
+	(Hızlı versiyon)
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{			
-				
+	{	
+		NumberUtilIsPrimeTest.run();	
+	}
+}
+
+class NumberUtilIsPrimeTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Bir sayı giriniz:");
+		int n = kb.nextInt();
+		
+		for (int i = 0; i <= n; ++i)
+			if (NumberUtil.isPrime(i))
+				System.out.println(i);		
+		
+		System.out.println(NumberUtil.isPrime(1_000_003));
+	}
+}
+
+class NumberUtil {
+	public static boolean isPrime(long a)
+	{
+		if (a <= 1)
+			return false;		
+		
+		if (a % 2 == 0)
+			return a == 2;
+		
+		if (a % 3 == 0)
+			return a == 3;
+		
+		if (a % 5 == 0)
+			return a == 5;
+		
+		if (a % 7 == 0)
+			return a == 7;
+
+		for (long i = 11; i * i <= a; i += 2)
+			if (a % i == 0)
+				return false;
+		
+		return true;
 	}
 }
 
