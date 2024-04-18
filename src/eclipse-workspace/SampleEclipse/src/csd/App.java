f1cd3e6b1e8548054101cf5e8689a51ee904622e
Oğuz Karan <oguzkaran@csystem.org>
2024-04-18 20:56:36 +0300

    Update

--- src/eclipse-workspace/SampleEclipse/src/csd/App.java
+++ src/eclipse-workspace/SampleEclipse/src/csd/App.java
@@ -1,49 +1,103 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden sayıdan büyük ilk Fibonacci sayısını döndüren nextFibonacciNumber
-	isimli metodu NumberUtil sınıfı içerisinde yazınız ve test ediniz	 	
+	switch deyimi:
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		NumberUtilFibonacciNumberTest.run();
+		DemoMenuApp.run();
 	}
 }
 
-class NumberUtilFibonacciNumberTest {
+class DemoMenuApp {
+	public static void printMenu()
+	{
+		System.out.println("1.Ekle");
+		System.out.println("2.Sil");
+		System.out.println("3.Güncelle");
+		System.out.println("4.Listele");
+		System.out.println("5.Çıkış");
+		System.out.print("Seçenek:");
+	}
+	
+	public static void printGoodbyeMessage()
+	{
+		System.out.println("-------------------------------------------");
+		System.out.println("C ve Sistem Programcıları Derneği");
+		System.out.println("Teşekkürler");
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void printInvalidOptionMessage()
+	{
+		System.out.println("-------------------------------------------");
+		System.out.println("Geçersiz seçenek!...");
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void insert()
+	{
+		System.out.println("-------------------------------------------");
+		System.out.println("Ekle seçildi");
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void delete()
+	{
+		System.out.println("-------------------------------------------");
+		System.out.println("Sil seçildi");
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void update()
+	{
+		System.out.println("-------------------------------------------");
+		System.out.println("Güncelle seçildi");
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void list()
+	{
+		System.out.println("-------------------------------------------");
+		System.out.println("Listele seçildi");
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void invalidOption()
+	{
+		printInvalidOptionMessage();
+	}
+	
+	public static void quit()
+	{
+		printGoodbyeMessage();
+		System.exit(0);
+	}
+	
+	public static void doOption(int option)
+	{
+		if (option == 1)
+			insert();
+		else if (option == 2)
+			delete();
+		else if (option == 3)
+			update();
+		else if (option == 4)
+			list();
+		else if (option == 5)
+			quit();
+		else
+			invalidOption();
+	}
+	
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		while (true) {
-			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
-			
-			if (n <= 0)
-				break;
-			
-			System.out.printf("%d. Fibonacci sayısı:%d%n", n, NumberUtil.fibonacciNumber(n));
+			printMenu();
+			doOption(Integer.parseInt(kb.nextLine()));			
 		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class NumberUtil {
-	public static int fibonacciNumber(int n)
-	{
-		if (n <= 2)
-			return n - 1;
-		
-		int prev1 = 1, prev2 = 0, result = prev1 + prev2;
-		
-		for (int i = 3; i < n; ++i) {
-			prev2 = prev1;
-			prev1 = result;
-			result = prev1 + prev2;
-		}
-		
-		return result;		
 	}
 }
