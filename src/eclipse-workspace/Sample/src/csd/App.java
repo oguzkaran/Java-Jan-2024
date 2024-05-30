573837a9435531a16403ee189a2e504ef8707e0b
Oğuz Karan <oguzkaran@csystem.org>
2024-05-30 22:39:04 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,7 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Nesne Yönelimli Programlama(Object Oriented Programming): Nesne yönelimli programlama tekniğini (NYPT) tek bir cümle
-	ile açıklamak pek mümkün değildir. Ancak, bu tekniği bilen veya belirli ölçüde fikri olan birisine "sınıflar kullanarak
-	program yazma tekniğidir" denir.  
+	  
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
