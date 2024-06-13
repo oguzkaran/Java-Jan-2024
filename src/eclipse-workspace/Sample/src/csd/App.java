151c70b1f5204ebb817cfbbcb0dfa921a64d551d
Oğuz Karan <oguzkaran@csystem.org>
2024-06-13 22:37:51 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,23 +1,87 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Aşağıdaki örnekte değer verilmemiş bir yerel değişkenin kullanılmasından dolayı error oluşur  
+	Yukarıdaki demo örnekte sınıflara count static veri elemanı eklenerek anlatılan problemler çözülebilir. 
+	
+	Anahtar Notlar: ** ve ** ile belirtilen kodların detayları şu an önemsizdir. Burada sayacın nesne yaratıldıkça
+	artırıldığına odaklanınız. Buradaki detaylar ileride ele alınacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		Sample s;
-		
-		
-	
-		
-		System.out.printf("s.x = %d, s.y = %b%n", s.x, s.y);
-	} 
+		DemoGameApp.run();
+	}
 }
 
-class Sample {
-	public int x;
-	public boolean y;
+class DemoGameApp {
+	public static void run()
+	{		
+		//...
+		
+		for (int i = 0; i < 10; ++i) {
+			Alien a = new Alien();		
+				
+			//...
+		}
+		
+		for (int i = 0; i < 20; ++i) {
+			Soldier soldier = new Soldier();		
+			
+			//...
+		}
+		
+		//...
+		
+		System.out.printf("Number of aliens:%d%n", Alien.count);
+		System.out.printf("Number of soldiers:%d%n", Soldier.count);			
+	}
+}
+
+class Alien {
+	public static int count;
+	public int color;
+	public int armCount;
 	
+	// ...
+	public Alien() //**
+	{
+		//...
+		++count;		
+	}
 	//...
 }
+
+class Soldier {
+	
+	public static int count;
+	public int title;
+	public int gunCount; 
+	 
+	 //...
+
+	
+	public Soldier() //***
+	{
+		++count;
+		//...
+	}
+	 
+	 //...
+}
+
+class Civilian {
+	public static int count;
+	//...
+}
+
+class Building {
+	public static int count;
+	//...
+}
+
+class Animal {
+	public static int count;
+	//...
+}
+
+//...
