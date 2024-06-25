b2812729b7ab2ad6326f6ffa5b81260fcae18610
Oğuz Karan <oguzkaran@csystem.org>
2024-06-25 20:45:52 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -3,7 +3,12 @@
 	bir referansın argüman olarak geçilmesi gerekir. Bu durumda metodun perametresi olan referans ile argüman olarak
 	geçilen referans aynı nesneyi gösterir duruma gelir. Anımsanacağı gibi argümanlardan parametre değişkenlerine 
 	aktarım da bir atama işlemidir. Bu durumda metot çağrısı boyunca ilgili nesneye erişilebilir. Hatta metot nesnenin
-	içeriğini de değiştirebilir.
+	içeriğini de değiştirebilir. Örnekte DateUtil sınıfının change metodunun nesnenin içeriğini değiştirdiğine dikkat 
+	ediniz. 
+	
+	Bir metodun geri dönüş değeri bir sınıf türünden olabilir. Bu durumda bir metot referansa (adrese) geri dçnmüş olur.
+	Böyle bir metodun return deyimine ilişkin ifadesi de aynı türden bir referans olmalıdır. Örnekteki create metodunun
+	bir nesne yaratıp o nesnenin adresine geri döndüğüne dikkat ediniz
 -----------------------------------------------------------------------------------------------------------------------*/
 
 package csd;
@@ -11,11 +16,7 @@ package csd;
 class App {
 	public static void main(String [] args) 
 	{	
-		Date d = new Date();
-		
-		d.day = 11;
-		d.month = 7;
-		d.year = 1983;
+		Date d = DateUtil.create(11, 7, 1983);
 		
 		DateUtil.display(d);
 		DateUtil.change(d, 6, 9, 2021);
@@ -30,6 +31,18 @@ class DateUtil {
 		date.day = day;
 		date.month = month;
 		date.year = year;
+	}
+	
+	public static Date create(int day, int month, int year)
+	{
+		//...
+		Date date = new Date();
+		
+		date.day = day;
+		date.month = month;
+		date.year = year;
+		
+		return date;
 	}
 	
 	public static void display(Date date)
