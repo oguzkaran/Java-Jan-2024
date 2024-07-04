d87aca8d7ddd3184884402e0f14ee9bd604a6cdb
Oğuz Karan <oguzkaran@csystem.org>
2024-07-04 22:55:37 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,130 +1,51 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Rassal (Random) Sayı Üretimi:
+	Sınıf Çalışması: Hilesiz iki zarın atılması deneyinde zarların ikisinin de çift sayı gelmesi olasılığını yaklaşık
+	olarak hesaplayan simülasyonu yazınız.
+	
+	Not: İleride daha iyisi yazılacaktır.
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		Complex z1, z2, z3;
-		
-		z1 = new Complex();
-		z2 = new Complex(-5.6);
-		z3 = new Complex(-5, 8);
-		
-		z1.display();
-		z2.display();
-		z3.display();
+		EvenDiceProbabilitySimulationApp.run();						
 	}
 }
 
-class Complex {
-	public double real;
-	public double imag;
-	
-	public static Complex add(double re1, double im1, double re2, double im2) //İleride sınıfı kullanan kodlardan gizleyeceğiz
+class EvenDiceProbabilitySimulationApp {
+	public static void run()
 	{
-		Complex result = new Complex();
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Input count:");
+		int count = kb.nextInt();
 		
-		result.real = re1 + re2;
-		result.imag = im1 + im2;
+		EvenDiceProbabilitySimulation simulation = new EvenDiceProbabilitySimulation();
 		
-		return result;
-	}
-	
-	public static Complex subtract(double re1, double im1, double re2, double im2) //İleride sınıfı kullanan kodlardan gizleyeceğiz
-	{
-		return add(re1, im1, -re2, -im2);		
-	}
-	
-	public Complex()
-	{
+		simulation.run(count);
 		
-	}
-	
-	public Complex(double a)
-	{
-		real = a;		
-	}
-	
-	public Complex(double a, double b)
-	{
-		real = a;
-		imag = b;
-	}
-	
-	public static Complex add(double val, Complex z)
-	{
-		return add(val, 0, z.real, z.imag);
-	}
-	
-	public Complex add(Complex other)
-	{
-		return add(real, imag, other.real, other.imag);
-	}
-	
-	public Complex add(double val)
-	{
-		return add(real, imag, val, 0);
-	}	
-	
-	public static Complex subtract(double val, Complex z)
-	{
-		return subtract(val, 0, z.real, z.imag);
-	}
-	
-	public Complex subtract(Complex other)
-	{
-		return subtract(real, imag, other.real, other.imag);
-	}
-	
-	public Complex subtract(double val)
-	{
-		return subtract(real, imag, val, 0);
-	}
-	
-	public void inc(double val)
-	{
-		real += val;
-	}
-	
-	public void inc()
-	{
-		inc(1);
-	}
-	
-	public void dec(double val)
-	{
-		inc(-val);
-	}
-	
-	public void dec()
-	{
-		dec(1);
-	}
-	
-	public Complex getConjugate()
-	{
-		Complex result = new Complex();
-		
-		result.real = real;
-		result.imag = -imag;
-		
-		return result;
-	}
-	
-	public double getNorm()
-	{
-		return Math.sqrt(real * real + imag * imag);
-	}
-	
-	public double getLength()
-	{
-		return getNorm();
-	}	
-	
-	public void display()
-	{
-		System.out.printf("(%.2f, %.2f)%n", real, imag);
+		System.out.printf("Probability:%f%n", simulation.p);
 	}
 }
+
+class EvenDiceProbabilitySimulation {
+	public double p;
+	
+	public static boolean areEven(java.util.Random r)
+	{
+		return (r.nextInt(6) + 1) % 2 == 0 && (r.nextInt(6) + 1) % 2 == 0;
+	}
+	
+	public void run(int count)
+	{
+		java.util.Random r = new java.util.Random();
+		int n = 0;
+		
+		for (int i = 0; i < count; ++i)
+			if (areEven(r))
+				++n;
+		
+		p = (double)n / count;		
+	}
+}
+
