7ca8d190261f783184483aba8b3a4a6f82384eb6
Oğuz Karan <oguzkaran@csystem.org>
2024-07-18 22:20:57 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,5 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örneği inceleyiniz
+	Sınıfı Çalışması: Aşağıda belirtilen metotları StrigUtil sınıfı içerisinde açıklamalara yazınız ve test ediniz.
+	Açıklamalar:
+	- Metotların prototipleri şunlardır:
+		public static String trim(String s);
+		public static String trimLeading(String s);
+		public static String trimTrailing(String s);
+	- Metotlar sırasıyla yazının başındaki ve sonundaki, yalnızca başındaki ve yalnızca sonundaki whitespace karakterleri
+	atacaklardır.
+	
+	- Metotlar Java 11 öncesi için yazılacaktır. Yani stripXXX metotları kullanılmayacaktır
 ---------------------------------------------------------------------------------------------------------------------*/
 
 package csd;
@@ -7,9 +16,21 @@ package csd;
 class App {
 	public static void main(String [] args) 
 	{		
-		System.out.println("Java" + 10 + 11); //Java1011
-		System.out.println(10 + 11 + "Java"); //21Java
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		
+		while (true) {
+			System.out.print("Input string:");
+			String s = kb.nextLine();
+			
+			System.out.printf("(%s)%n", s);
+			System.out.printf("(%s)%n", s.trim());
+			System.out.printf("(%s)%n", s.strip());
+			System.out.printf("(%s)%n", s.stripLeading());
+			System.out.printf("(%s)%n", s.stripTrailing());
+			
+			if ("exit".equals(s))
+				break;
+		}
 	}
 }
-
-
