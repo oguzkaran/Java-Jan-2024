d18d8cf1c7838153ded44e0e216a0986d69be980
Oğuz Karan <oguzkaran@csystem.org>
2024-07-23 21:43:10 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,14 +1,16 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıfı Çalışması: Aşağıda belirtilen metotları StrigUtil sınıfı içerisinde açıklamalara yazınız ve test ediniz.
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının pangram olup olmadığını test eden isPangramTR ve isPangramEN 
+	isimli metotları yazınız ve aşağıdaki kod ile test ediniz.
 	Açıklamalar:
-	- Metotların prototipleri şunlardır:
-		public static String trim(String s);
-		public static String trimLeading(String s);
-		public static String trimTrailing(String s);
-	- Metotlar sırasıyla yazının başındaki ve sonundaki, yalnızca başındaki ve yalnızca sonundaki whitespace karakterleri
-	atacaklardır.
-	
-	- Metotlar Java 11 öncesi için yazılacaktır. Yani stripXXX metotları kullanılmayacaktır
+		- İlgili dilin alfadbesindeki tüm karakterler kullanılarak oluşturulan ve içerisinde özel isim olmayan anlamlı 
+		cümlelere "pangram" denir. 
+		Örneğin tipik bir İnglizce pangram şudur:		
+			The quick brown fox jumps over the lazy dog.
+			
+		Örneğin tipik bir Türkçe pangram şudur:
+			Pijamalı hasta yağız şoföre çabucak güvendi.
+		
+		- Metotlar cümlenin anlamına ve özel isim içerip içermediğine bakmayacaktır
 ---------------------------------------------------------------------------------------------------------------------*/
 
 package csd;
@@ -16,21 +18,59 @@ package csd;
 class App {
 	public static void main(String [] args) 
 	{		
+		StringUtilIsPangramTest.run();
+	}
+}
+
+class StringUtilIsPangramTest {
+	public static void run()
+	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		
+		StringUtilIsPangramTRTest.run(kb);
+		StringUtilIsPangramENTest.run(kb);
+	}
+}
+
+class StringUtilIsPangramTRTest {
+	public static void run(java.util.Scanner kb)
+	{
+		while (true) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
+			
+			if ("elma".equals(s))
+				break;
+			
+			System.out.println(StringUtil.isPangramTR(s) ? "Pangram" : "Pangram değil");			
+		}
+	}
+}
+
+
+class StringUtilIsPangramENTest {
+	public static void run(java.util.Scanner kb)
+	{		
 		while (true) {
 			System.out.print("Input string:");
 			String s = kb.nextLine();
 			
-			System.out.printf("(%s)%n", s);
-			System.out.printf("(%s)%n", s.trim());
-			System.out.printf("(%s)%n", s.strip());
-			System.out.printf("(%s)%n", s.stripLeading());
-			System.out.printf("(%s)%n", s.stripTrailing());
-			
 			if ("exit".equals(s))
 				break;
+			
+			System.out.println(StringUtil.isPangramEN(s) ? "Pangram" : "Not pangram");			
 		}
+	}
+}
+
+class StringUtil {
+	public static boolean isPangramTR(String s)
+	{
+		//TODO
+	}
+	
+	public static boolean isPangramEN(String s)
+	{
+		//TODO
 	}
 }
