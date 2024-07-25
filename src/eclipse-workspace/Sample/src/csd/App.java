8f8d0435951eaba668c3d2710e1381c580a343ac
Oğuz Karan <oguzkaran@csystem.org>
2024-07-25 22:20:00 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,11 +1,27 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	
+	Yukarıdaki örnek aşağıdaki gibi yazılmalıdır
 ---------------------------------------------------------------------------------------------------------------------*/
+
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
+	
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		while (true) {
+			System.out.print("Input name:");
+			String name = kb.nextLine();
+			
+			if ("exit".equals(name))
+				break;
+			
+			System.out.print("Input number:");
+			int no = Integer.parseInt(kb.nextLine());
+			
+			System.out.printf("%d, %s%n", no, name);
+		}		
 	}
 }
+
