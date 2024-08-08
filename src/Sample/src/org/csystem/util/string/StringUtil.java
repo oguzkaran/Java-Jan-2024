d33257dcc19d2c236c23e6c30d327aefe245d017
Oğuz Karan <oguzkaran@csystem.org>
2024-08-08 15:43:17 +0300

    Update StringUtil.java

--- src/Sample/src/org/csystem/util/string/StringUtil.java
+++ src/Sample/src/org/csystem/util/string/StringUtil.java
@@ -6,19 +6,148 @@
 package org.csystem.util.string;
 
 public class StringUtil {
-	public static int countString(String s1, String s2)
-	{
-		int count = 0;
-		int idx = 0;
-		
-		
-		while ((idx = s1.indexOf(s2, idx)) != -1) {
-			++count;
-			++idx;
+	public static String capitalize(String s) {
+		return s.isEmpty() ? s : Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
+	}
+
+
+	public static String changeCase(String s) {
+		StringBuilder sb = new StringBuilder(s);
+
+		for (int i = 0; i < s.length(); ++i) {
+			char c = s.charAt(i);
+
+			sb.setCharAt(i, Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c));
 		}
-		
-		
+
+		return sb.toString();
+	}
+
+
+	public static int countString(String s1, String s2) {
+		int count = 0;
+
+		for (int i = 0; (i = s1.indexOf(s2, i)) != -1; ++i, ++count)
+			;
+
 		return count;
 	}
-	//...
+
+
+	public static boolean isPalindrome(String s)
+	{
+		int left = 0;
+		int right = s.length() - 1;
+
+		while (left < right) {
+			char cLeft = s.charAt(left);
+
+			if (!Character.isLetter(cLeft)) {
+				++left;
+				continue;
+			}
+
+			char cRight = s.charAt(right);
+
+			if (!Character.isLetter(cRight)) {
+				--right;
+				continue;
+			}
+
+			if (Character.toLowerCase(cLeft) != Character.toLowerCase(cRight))
+				return false;
+
+			++left;
+			--right;
+		}
+
+		return true;
+	}
+
+
+	public static boolean isPangram(String s, String alphabet) {
+		for (int i = 0; i < alphabet.length(); ++i)
+			if (s.indexOf(alphabet.charAt(i)) == -1)
+				return false;
+
+		return true;
+	}
+
+
+	public static boolean isPangramEN(String s) {
+		s = s.toLowerCase();
+
+		for (char c = 'a'; c <= 'z'; ++c)
+			if (s.indexOf(c) == -1)
+				return false;
+
+		return true;
+	}
+
+
+
+	public static boolean isPangramTR(String s) {
+		return isPangram(s.toLowerCase(), "abcçdefgğhıijklmnoöprsştuüvyz");
+	}
+
+
+	public static String padLeading(String s, int n, char ch) {
+		int len = s.length();
+
+		return len < n ? String.valueOf(ch).repeat(n - len) + s : s;
+	}
+
+
+	public static String padLeading(String s, int n) {
+		return padLeading(s, n, ' ');
+	}
+
+
+	public static String padTrailing(String s, int n, char ch) {
+		int len = s.length();
+
+		return len < n ? s + String.valueOf(ch).repeat(n - len) : s;
+	}
+
+
+	public static String padTrailing(String s, int n) {
+		return padTrailing(s, n, ' ');
+	}
+
+
+	public static String repeat(int count, char ch)
+	{
+		return ("%0" + count + "d").formatted(0).replace('0', ch);
+	}
+
+
+	public static String reverse(String s) {
+		return new StringBuilder(s).reverse().toString();
+	}
+
+
+	public static String trim(String s) {
+		return trimTrailing(trimLeading(s));
+	}
+
+
+	public static String trimLeading(String s) {
+		int i = 0;
+
+		for (; i < s.length() && Character.isWhitespace(s.charAt(i)); ++i)
+			;
+
+		return s.substring(i);
+	}
+
+
+	public static String trimTrailing(String s) {
+		int i = s.length() - 1;
+
+		for (; i >= 0 && Character.isWhitespace(s.charAt(i)); --i)
+			;
+
+		return s.substring(0, i + 1);
+	}
+
 }
