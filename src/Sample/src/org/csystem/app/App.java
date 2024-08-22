c14ad44a94c777b0b1852ef30a05cb274ed6fd32
Oğuz Karan <oguzkaran@csystem.org>
2024-08-22 20:03:15 +0300

    update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,11 +1,30 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir metodun parametre değişkeni bir dizi referansı olabilir
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir dizinin elemanları toplamına geri dönen sum metodunu ArrayUtil
+	sınıfı içerisinde yazınız. Metodunuzu rassal olarak üretilmiş diziler ile test ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import static org.csystem.util.array.ArrayUtil.generateRandomArray;
+import static org.csystem.util.array.ArrayUtil.print;
+
+import java.util.Random;
+import java.util.Scanner;
 
 class App {
 	public static void main(String[] args)
 	{
-		
+		Scanner kb = new Scanner(System.in);
+		Random random = new Random();
+
+		while (true) {
+			System.out.print("Bir sayı giriniz:");
+			int count = kb.nextInt();
+			if (count <= 0)
+				break;
+
+			double [] a = generateRandomArray(random, count, 0., 100);
+
+			print(a);
+		}
 	}
 }
--- src/Sample/src/org/csystem/util/array/ArrayUtil.java
+++ src/Sample/src/org/csystem/util/array/ArrayUtil.java
@@ -1,11 +1,33 @@
 /**
  * Utility class for array operations
- * Last Update: 20th August 2024
+ * Last Update: 22nd August 2024
  * @author Java-Jan-2024 Group
  */
 package org.csystem.util.array;
 
+import java.util.Random;
+
 public class ArrayUtil {
+    public static int [] generateRandomArray(Random random, int count, int origin, int bound)
+    {
+        int [] a = new int[count];
+
+        for (int i = 0; i < count; ++i)
+            a[i] = random.nextInt(origin, bound);
+
+        return a;
+    }
+
+    public static double [] generateRandomArray(Random random, int count, double origin, double bound)
+    {
+        double [] a = new double[count];
+
+        for (int i = 0; i < count; ++i)
+            a[i] = random.nextDouble(origin, bound);
+
+        return a;
+    }
+
     public static void print(int [] a)
     {
         print(a, ' ', '\n');
@@ -13,8 +35,33 @@ public class ArrayUtil {
 
     public static void print(int [] a, char sep, char end)
     {
+        print(a, 1, sep, end);
+    }
+
+    public static void print(int [] a, int n)
+    {
+        print(a, n, ' ', '\n');
+    }
+
+    public static void print(int [] a, int n, char sep, char end)
+    {
+        String fmt = String.format("%%0%dd%c", n, sep);
+
         for (int i = 0; i < a.length; ++i)
-            System.out.printf("%d%c", a[i], sep);
+            System.out.printf(fmt, a[i], sep);
+
+        System.out.print(end);
+    }
+
+    public static void print(double [] a)
+    {
+        print(a, '\n', '\n');
+    }
+
+    public static void print(double [] a, char sep, char end)
+    {
+        for (int i = 0; i < a.length; ++i)
+            System.out.printf("%f%c", a[i], sep);
 
         System.out.print(end);
     }
