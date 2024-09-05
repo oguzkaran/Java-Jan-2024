7883b7f221d857e66e9c481e7cc225c82c41ba33
Oğuz Karan <oguzkaran@csystem.org>
2024-09-05 20:31:47 +0300

    update for study case

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,13 +1,36 @@
 package org.csystem.app;
 
+import org.csystem.util.string.StringUtil;
+
+import java.util.Scanner;
+
 class App {
 	public static void main(String [] args)
 	{
-		String text = "Bugün hava çok güzel, çok çok güzel";
-		String s = "çok";
+		ChangeCaseTest.run();;
+	}
+}
 
-		System.out.println(text.lastIndexOf(s));
-		System.out.println(text.lastIndexOf(s, 12));
-		System.out.println(text.lastIndexOf(s, 27));
+class ChangeCaseTest {
+	public static void run()
+	{
+		Scanner kb = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("Input string:");
+			String s = kb.nextLine();
+
+			System.out.printf("%s%n", StringUtil.changeCase(s));
+
+			if ("exit".equals(s))
+				break;
+		}
+	}
+}
+
+class Util {
+	public static String changeCase(String s)
+	{
+		//TODO
 	}
 }
