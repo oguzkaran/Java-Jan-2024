aceedfa33fa3c84586119588f9e8d0a0906b83a0
Oğuz Karan <oguzkaran@csystem.org>
2025-07-08 20:50:36 +0300

    update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -3,21 +3,6 @@ package org.csystem.app;
 class App {
     public static void main(String[] args)
     {
-        Integer i = 10;
-        X.foo(10);
-        X.foo(i);
+
     }
 }
-
-class X {
-    public static void foo(int a)
-    {
-        System.out.println("foo, int");
-    }
-}
-
-/*
-1. Sentaks hatasıdır
-2. foo, int yazılır
-3. foo, Integer yazılır
- */
